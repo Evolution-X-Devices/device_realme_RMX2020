f4c744b4483a2fde96d7e4a3ccfdf6b6dd340a67
Koushik Dey <kousickdey3@gmail.com>
2023-06-22 21:50:17 +0530

    RMX2020: Drop `QUICK_TAP` and `LIVE_WALLPAPERS` Flags * forget to remove
    Signed-off-by: Koushik Dey <kousickdey3@gmail.com>

--- evolution_RMX2020.mk
+++ evolution_RMX2020.mk
@@ -17,8 +17,6 @@ $(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 EVO_BUILD_TYPE := OFFICIAL
 TARGET_BOOT_ANIMATION_RES := 720
 TARGET_USES_MINI_GAPPS := true
-TARGET_SUPPORTS_QUICK_TAP := false
-TARGET_INCLUDE_LIVE_WALLPAPERS := false
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := evolution_RMX2020
